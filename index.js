720f9350e669d97dc97351a172aa6081593a2155
Bowen Wang <iwangbowen@gmail.com>
2018-12-18 11:31:17 +0800

    Update code

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+// The one and only way of getting global scope in all environments
+// https://stackoverflow.com/q/3277182/1008999
+var _global = typeof window === 'object' && window.window === window
+    ? window : typeof self === 'object' && self.self === self
+        ? self : typeof global === 'object' && global.global === global
+            ? global
+            : this
 
 require('babel-polyfill');
-const ExcelJS = require('./node_modules/exceljs/dist/es5/exceljs.browser');
+const Excel = require('./node_modules/exceljs/dist/es5/exceljs.browser');
 const FileSaver = require('file-saver');
+const saveAs = FileSaver.saveAs;
 
-window.ExcelJS = ExcelJS;
-window.FileSaver = FileSaver;
+_global.Excel = Excel;
+_global.saveAs = saveAs;
+
+if (typeof module !== 'undefined') {
+    module.exports = {
+        Excel,
+        saveAs
+    };
+}
